46bc2599c59a6844c53df65e79a377596124a6b3
MorsTemplar <msaqlain8488@gmail.com>
2025-05-05 22:22:05 +0500

    worked with js file of footer

---
+++ SRC/layout/footer/index.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <Box className={styles.footer}>
+      <Typography variant="body2" component="p">
+        © {new Date().getFullYear()} Your Name. All rights reserved.
+      </Typography>
+    </Box>
+  );
+}
