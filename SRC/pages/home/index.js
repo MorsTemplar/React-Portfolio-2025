535c899d722d7c874d2e52662c6e2b1d76262d8b
MorsTemplar <msaqlain8488@gmail.com>
2025-05-05 23:00:06 +0500

    worked with js file of home

---
+++ SRC/pages/home/index.js
@@ -0,0 +1,18 @@
+import { Box, Typography } from '@mui/material';
+import styles from './Home.module.css';
+
+export default function Home() {
+  return (
+    <Box className={styles.container}>
+      <Typography variant="h2" className={styles.heading}>
+        Welcome to My Portfolio
+      </Typography>
+      <Typography variant="h5" className={styles.subheading}>
+        Frontend Developer & UI Enthusiast
+      </Typography>
+      <Typography variant="body1" className={styles.description}>
+        Creating beautiful and functional web experiences with React and Material-UI
+      </Typography>
+    </Box>
+  );
+}
